0086f391409cb5533c0b07d2aa68035ab8f15d4a
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-11-20 20:45:45 -0300

    UP1.py

--- webapp00.py
+++ webapp00.py
@@ -1,29 +1,12 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
-from MinhasFuncoes import *
 
-area = AreaTriangulo(3,6)
-st.write("A área do triângulo é: ", area)
+form = st.form("formHD")
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu nome:")
+email = st.text_input("Digite seu e-mail:")
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "Critico"))
+assunto = st.text_input("Assunto: ")
+mensagem = st.text_input("Messagem: ")
+arquivos = st.file_uploader('Uploader de Arquivo')
 
-
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Meu prof. Massaki é D+ e muito convencido!")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-col2, col3 = st.columns(2)
-
-with col2:
-    st.header("NALA")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("CORUJA")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Enviar?usp=pp_url&entry")
