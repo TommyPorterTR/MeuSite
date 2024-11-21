578a4ac286c22af909f205aba106c7ea4bac8e59
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-11-20 21:22:08 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,17 +1,13 @@
+# MEU PRIMEIRO WEB APP
 import streamlit as st
-import pandas as pd
-import urllib3
-from urllib3 import request
-from io import BytesIO
-import request
 
 form = st.form("formHD")
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
 email = st.text_input("Digite seu e-mail:")
-prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "Critico"))
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
 assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 arquivos = st.file_uploader('Uploader de Arquivo')
 
-rd = request.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQNW3ECuL6022Cg-ZUAiC4eHpW6INB2PLhtm_8jiWHTkdNdkhYEB71XptIbooT_pLmjURLz5ihSLBkH/pub?gid=812473058&single=true&output=csv')
+botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"&entry.288382773="+arquivos+"")
