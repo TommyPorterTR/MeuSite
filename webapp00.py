1b740ba41d5c87fce31525bc30e292a5ef150fa4
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-11-20 21:46:36 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -8,6 +8,5 @@ email = st.text_input("Digite seu e-mail:")
 prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
 assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
-arquivos = st.file_uploader('Uploader de Arquivo')
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
