9e8cde2ac686378b21a6e24460b7805cd232f91b
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-11-20 21:21:37 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -14,6 +14,4 @@ assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 arquivos = st.file_uploader('Uploader de Arquivo')
 
-botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Enviar?usp=pp_url&entry")
-
 rd = request.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQNW3ECuL6022Cg-ZUAiC4eHpW6INB2PLhtm_8jiWHTkdNdkhYEB71XptIbooT_pLmjURLz5ihSLBkH/pub?gid=812473058&single=true&output=csv')
