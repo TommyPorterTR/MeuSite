d78c1135d10027a0b6a6c36d8abaf8d906ec40e9
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-12-04 22:42:45 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,12 +1,21 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
 
-form = st.form("formHD")
-ID = st.text_input("Digite seu ID:")
-nome = st.text_input("Digite seu nome:")
-email = st.text_input("Digite seu e-mail:")
-prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
-assunto = st.text_input("Assunto: ")
-mensagem = st.text_input("Messagem: ")
+@st.dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
 
-botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu Nome:")
+Senha = st.text_input("Digite sua Senha:")
+
+
+if "vote" not in st.session_state:
+    if st.button("Login"): 
+        if nome == 'admin' and senha == 'admin':
+            vote(login)
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
