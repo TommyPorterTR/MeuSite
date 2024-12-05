343894c2c3d32692ea5058b037560e579e5a563a
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-12-04 22:57:58 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -20,3 +20,13 @@ if "vote" not in st.session_state:
             st.success("Login bem-sucedido!")
         else:
             st.error("Nome de usuário ou senha incorretos.")
+
+
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu nome:")
+email = st.text_input("Digite seu e-mail:")
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
+assunto = st.text_input("Assunto: ")
+mensagem = st.text_input("Messagem: ")
+
+botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
