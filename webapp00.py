c45047222f3e8b9f44046438e549ae4c87bf2d4f
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-12-05 00:01:25 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -11,7 +11,7 @@ custom_html = f"""
     <style>
         body {{
             background-image: url('{image_url}');
-            background-size: 800px 200px;  /* Define largura de 200px e altura de 100px */
+            background-size: 900px 200px;  /* Define largura de 200px e altura de 100px */
 
 
         }}
