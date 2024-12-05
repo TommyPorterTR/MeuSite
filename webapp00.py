89ebe87595ba46511f849ba0090176a7e50346f3
TommyPorterTR <90474801+TommyPorterTR@users.noreply.github.com>
2024-12-04 23:22:10 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -66,7 +66,7 @@ def form_page():
         st.markdown(f'<a href="{url}" target="_blank">Clique aqui para enviar seus dados</a>', unsafe_allow_html=True)
 
 # Adiciona a imagem de fundo (coloque a URL da imagem desejada entre aspas)
-image_url = "https://assets.folhavitoria.com.br/images/a0322d70-1f5c-11ef-9ec9-9f1d3c6a39fe--minified.jpg"  # Substitua com a URL correta da imagem
+image_url = "https://meialua.net/wp-content/uploads/2024/06/tectoy.png"  # Substitua com a URL correta da imagem
 add_background_image(image_url)
 
 # Verifica se o usuário está logado e decide qual página exibir
